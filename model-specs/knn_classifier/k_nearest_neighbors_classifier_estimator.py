107eec0e733794dbffbcf58256506abc174b8c85
KienMN <kienmn97@gmail.com>
2019-07-08 11:04:48 +0000

    andd knn clf, log reg clf, random forest clf spec

---
+++ model-specs/knn_classifier/k_nearest_neighbors_classifier_estimator.py
@@ -0,0 +1,12 @@
+from wi_ml_toolkit.classifier import KNearestNeighborsClassifier as WiEstimator
+
+#class WiEstimator():
+#    '''
+#    TODO: your code for your estimator goes here.
+#    Your estimator should be compatible with sklearn.Pipeline
+#    '''
+#    def fit(self, X, y, *args):
+#        pass
+#    def predict(self, X, *args):
+#        pass
+
