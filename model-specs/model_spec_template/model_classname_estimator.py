4b61d9724f94bca90a7ff021d4518fefc2d4544e
Kien Mai Ngoc <kienmn97@gmail.com>
2019-07-09 15:35:32 +0700

    Update spec for regression model classes

---
+++ model-specs/model_spec_template/model_classname_estimator.py
@@ -0,0 +1,15 @@
+# Importing model class as the name WiEstimator.
+# For example:
+# from sklearn.linear_model import Lasso as WiEstimator
+
+# Or writing WiEstimator class.
+# class WiEstimator():
+#    '''
+#    TODO: your code for your estimator goes here.
+#    Your estimator should be compatible with sklearn.Pipeline
+#    '''
+#    def fit(self, X, y):
+#        pass
+#    def predict(self, X):
+#        pass
+
