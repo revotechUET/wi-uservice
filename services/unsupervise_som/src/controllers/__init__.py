5c301a3d3953d6d4c7a389fd30ddc8ac0dfcf7da
ThiinhUET <thinhlevan2015@gmail.com>
2020-04-15 16:36:10 +0700

    take time

--- services/unsupervise_som/src/controllers/__init__.py
+++ services/unsupervise_som/src/controllers/__init__.py
@@ -97,7 +97,9 @@ def get_model(model_id):
 
         inversed_competitive_layer_weights = scaler.inverse_transform(model._competitive_layer_weights)
         scaled_competitive_layer_weights = inversed_competitive_layer_weights / np.amax(inversed_competitive_layer_weights, axis = 0)
-        print("-----------" + model.__dict__)
+        bug = vars(model)
+        for item in bug:
+            print(item, ':', bug[item])
         tmp_label = model._nodes_label.copy().reshape(n_rows, n_cols)
         distribution_maps_data = []
         visualization_map_data = []
