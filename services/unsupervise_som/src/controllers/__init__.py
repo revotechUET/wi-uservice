0d1542ab5bd7f12bea8af610eb0ee089ad962e1e
ThiinhUET <thinhlevan2015@gmail.com>
2020-04-15 16:16:31 +0700

    update

--- services/unsupervise_som/src/controllers/__init__.py
+++ services/unsupervise_som/src/controllers/__init__.py
@@ -97,9 +97,8 @@ def get_model(model_id):
 
         inversed_competitive_layer_weights = scaler.inverse_transform(model._competitive_layer_weights)
         scaled_competitive_layer_weights = inversed_competitive_layer_weights / np.amax(inversed_competitive_layer_weights, axis = 0)
-
-        tmp_label = model._nodes_label.copy().reshape(n_rows, n_cols)
         print("-----------" + model)
+        tmp_label = model._nodes_label.copy().reshape(n_rows, n_cols)
         distribution_maps_data = []
         visualization_map_data = []
 
