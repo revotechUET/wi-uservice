b096315093466b58010b427a6f14a0d9f2ac84b3
ThiinhUET <thinhlevan2015@gmail.com>
2020-04-15 15:44:32 +0700

    cluster -> _node_label

--- services/unsupervise_som/src/controllers/__init__.py
+++ services/unsupervise_som/src/controllers/__init__.py
@@ -98,7 +98,7 @@ def get_model(model_id):
         inversed_competitive_layer_weights = scaler.inverse_transform(model._competitive_layer_weights)
         scaled_competitive_layer_weights = inversed_competitive_layer_weights / np.amax(inversed_competitive_layer_weights, axis = 0)
 
-        tmp_label = model.cluster_label.copy().reshape(n_rows, n_cols)
+        tmp_label = model._nodes_label.copy().reshape(n_rows, n_cols)
 
         distribution_maps_data = []
         visualization_map_data = []
@@ -170,7 +170,7 @@ def get_model(model_id):
 
         fitted_model = {
             "inversedCompetitiveWeights": inversed_competitive_layer_weights.tolist(),
-            "nodesLabel": model.node_label.tolist()
+            "nodesLabel": model._node_label.tolist()
         }   
 
         success_message.add("fitted_model", fitted_model)
