9cfc399a7c87f877a602f99d3a52e12fcace6de7
ThiinhUET <thinhlevan2015@gmail.com>
2020-04-23 23:04:54 +0700

    validator svm

--- services/svm_regression/src/ml_models/validator.py
+++ services/svm_regression/src/ml_models/validator.py
@@ -1,3 +1,8 @@
+def type_enforce(type, value):
+    if type == int or type == float or type == str:
+        return type(value)
+    else:
+        return value
 class BaseValidator(object):
     # def __init__(self, **kwargs):
     #     self.props = None
@@ -7,7 +12,7 @@ class BaseValidator(object):
         assert self.props is not None, "Empty predefined property list. Predefined properties are not set"
         for prop in self.props:
             if prop["name"] in kwargs:
-                setattr(self, 'prop_'+prop["name"], kwargs[prop["name"]])
+                setattr(self, 'prop_'+prop["name"], type_enforce(prop["type"], kwargs[prop["name"]]) )
 
     def __call__(self):
         assert self.props is not None, "Empty predefined property list. Predefined properties are not set"
